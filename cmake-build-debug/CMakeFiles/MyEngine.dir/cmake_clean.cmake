45e58452c312de29907fe8e7033f7a4b5b315d82
Murat <dautovmurat5@gmail.com>
2020-07-21 14:39:26 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/MyEngine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyEngine.dir/main.cpp.o"
+  "MyEngine"
+  "MyEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
